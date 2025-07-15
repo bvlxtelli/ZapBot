923cacd02537680dda7e5d0d1ed8efee1f1b0c94
Matheus Moura <matheus.moura@bistek.com.br>
2025-07-15 15:58:33 -0300

    Remoção de arquivos desnecessários

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# modules/__init__.py
-
-from my_lib import *
-
-from .inventario import inventarios
-from .qbc import quebra_conhecida
-from .pdf_creator import gerar_pdf
--- my_lib/__init__.py
+++
@@ -1,5 +0,0 @@
-# my_lib/__init__.py
-from .my_vars import *
-from .paths import *
-from .get_rel import *
-from .funcoes import *
